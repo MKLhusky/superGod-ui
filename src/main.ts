ac9f12c185cf4f73f79af920f64291255c1ff643
范文鹏 <fanwenpeng@51rcar.com>
2023-04-01 17:56:44 +0800

    1、初始化前端仓库

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { createApp } from 'vue'
+import 'element-plus/theme-chalk/index.css'
+import './assets/global.css'
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
+import {ElInput, ElForm, ElButton, ElFormItem } from 'element-plus'
+import ElementUI from 'element-plus'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElInput)
+app.use(ElementUI)
+app.use(ElForm)
+app.use(ElFormItem)
+app.use(ElButton)
 
 app.mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="loginbody">
+    <div class="logindata">
+      <div class="logintext">
+        <h2>剑来</h2>
+      </div>
+      <div class="formdata">
+        <el-form ref="form" :model="form" :rules="rules">
+          <el-form-item prop="username">
+            <el-input
+                v-model="form.username"
+                clearable
+                placeholder="请输入账号"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+                v-model="form.password"
+                clearable
+                placeholder="请输入密码"
+                show-password
+            ></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+
+      <div class="butt">
+        <el-button type="primary"
+        >登录</el-button
+        >
+        <el-button class="shou" @click="register">注册</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "login",
+  data() {
+    return {
+      form: {
+        password: "",
+        username: "",
+      },
+      checked: false,
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { max: 10, message: "不能大于10个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { max: 10, message: "不能大于10个字符", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped>
+.loginbody {
+  width: 100%;
+  height: 100%;
+  min-width: 100%;
+  background-image: url("../assets/1679660288277.jpg");
+  background-size: 100% 100%;
+  background-position: center center;
+  overflow: auto;
+  background-repeat: no-repeat;
+  position: fixed;
+  line-height: 100%;
+  padding-top: -8px;
+}
+
+.logintext {
+  margin-bottom: 20px;
+  line-height: 50px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: bolder;
+  color: white;
+  text-shadow: 2px 2px 4px #000000;
+}
+
+.logindata {
+  width: 400px;
+  height: 300px;
+  transform: translate(-50%);
+  margin-left: 50%;
+}
+
+.tool {
+  display: flex;
+  justify-content: space-between;
+  color: #606266;
+}
+
+.butt {
+  margin-top: 10px;
+  text-align: center;
+}
+
+.shou {
+  cursor: pointer;
+  color: #606266;
+}
+
+/*ui*/
+/* /deep/ .el-form-item__label {
+  font-weight: bolder;
+  font-size: 15px;
+  text-align: left;
+}
+
+/deep/ .el-button {
+  width: 100%;
+  margin-bottom: 10px;
+
+} */
+</style>
